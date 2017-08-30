ef1b9029319216cd64c87cf74d04849a5e4b37a9
Ivan Ramirez <ivan@astrochasqui.com>
2017-08-30 11:42:36 -0700

    added docstring

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 
 def do_something():
+    '''Prints a questionable math statement.
+    '''
     something = "2 + 2 = 5"
     print(something)# this will make it much easier in future problems to see that something is actually happening
